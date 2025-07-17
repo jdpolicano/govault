cfc8c7aed5969ccaddb9699584e053df33369e32
Jake <jacobpolicano@northwesternmutual.com>
2025-07-17 12:18:28 -0400

    style: add some helpers

--- internal/server/routes/create.go
+++ internal/server/routes/create.go
@@ -2,7 +2,6 @@ package routes
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/jdpolicano/govault/internal/server"
@@ -19,68 +18,80 @@ type CreateRequest struct {
 	Password string `json:"password"`
 }
 
+// GetCreateRoute returns an HTTP handler function for creating a new user and session.
 func GetCreateRoute(ctx *server.Context) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
-		body, e := validateBody(req)
-		if e != nil {
+		reqData, err := validateRequest(req)
+		if err != nil {
 			server.JSONResponse(w, server.NewInvalidBodyError())
 			return
 		}
 
-		username, password := body.Username, body.Password
-		user, salt, e := vault.CreateUserFromPW(username, password)
-		if e != nil {
-			ctx.Log.Println(e)
-			server.JSONResponse(w, server.NewServerError(e))
+		user, salt, err := vault.CreateUserFromPW(reqData.Username, reqData.Password)
+		if err != nil {
+			ctx.Log.Printf("error creating user %s: %v", reqData.Username, err)
+			server.JSONResponse(w, server.NewServerError(err))
+			return
+		}
+		ctx.Log.Printf("successfully derived salt for user \"%s\"", reqData.Username)
+
+		if err = ctx.Store.AddUser(user); err != nil {
+			ctx.Log.Printf("error adding user %s: %v", reqData.Username, err)
+			routeStoreError(w, err)
+			return
+		}
+		ctx.Log.Printf("successfully added user \"%s\"", reqData.Username)
+
+		token, err := createUserSession(ctx, user, reqData.Password, salt)
+		if err != nil {
+			ctx.Log.Printf("error creating session for user %s: %v", reqData.Username, err)
+			server.JSONResponse(w, server.NewServerError(err))
 			return
 		}
 
-		ctx.Log.Printf("successfully derived salt for user \"%s\"", username)
-
-		if e := ctx.Store.AddUser(user); e != nil {
-			ctx.Log.Println(e)
-			switch e.(type) {
-			case store.UserAlreadyExistsError:
-				{
-					server.JSONResponse(w, server.NewClientError(e))
-				}
-			default:
-				{
-
-					server.JSONResponse(w, server.NewServerError(e))
-				}
-			}
-			return
-		}
-
-		ctx.Log.Printf("successfully added user \"%s\"", username)
-		// create a session and return the token.
-		sessId, idErr := server.GenerateSessionID()
-		if idErr != nil {
-			fmt.Println(idErr)
-			server.JSONResponse(w, server.NewServerError(idErr))
-			return
-		}
-
-		ctx.Log.Printf("starting session id=\"%s\"", sessId)
-		aesKey, ciphErr := vault.CreateTextKeyFromPW("aes", password, salt)
-		if ciphErr != nil {
-			fmt.Println(idErr)
-			server.JSONResponse(w, server.NewServerError(ciphErr))
-			return
-		}
-
-		sess := server.NewSession(user.Name, aesKey, ctx.Config.DefaultTTL)
-		ctx.Sessions.Set(sessId, sess)
-		res := CreateSuccess{Token: sessId}
-		server.JSONResponse(w, server.NewServerSuccess(res))
+		sendToken(w, token)
 		ctx.Log.Println("response sent")
 	}
 }
 
-func validateBody(req *http.Request) (CreateRequest, error) {
-	var loginReq CreateRequest
+// validateRequest decodes and validates the request body.
+func validateRequest(req *http.Request) (CreateRequest, error) {
+	var reqData CreateRequest
 	decoder := json.NewDecoder(req.Body)
-	e := decoder.Decode(&loginReq)
-	return loginReq, e
+	err := decoder.Decode(&reqData)
+	return reqData, err
+}
+
+// createUserSession encapsulates session setup logic.
+func createUserSession(ctx *server.Context, user store.User, password string, salt []byte) (string, error) {
+	sessId, err := server.GenerateSessionID()
+	if err != nil {
+		return "", err
+	}
+	ctx.Log.Printf("starting session id=\"%s\"", sessId)
+
+	aesKey, err := vault.CreateTextKeyFromPW("aes", password, salt)
+	if err != nil {
+		return "", err
+	}
+
+	sess := server.NewSession(user.Name, aesKey, ctx.Config.DefaultTTL)
+	ctx.Sessions.Set(sessId, sess)
+	return sessId, nil
+}
+
+// sendToken sends a successful response with the session token.
+func sendToken(w http.ResponseWriter, token string) {
+	res := CreateSuccess{Token: token}
+	server.JSONResponse(w, server.NewServerSuccess(res))
+}
+
+// routeStoreError handles different store error types.
+func routeStoreError(w http.ResponseWriter, err error) {
+	switch err.(type) {
+	case store.UserAlreadyExistsError:
+		server.JSONResponse(w, server.NewClientError(err))
+	default:
+		server.JSONResponse(w, server.NewServerError(err))
+	}
 }
